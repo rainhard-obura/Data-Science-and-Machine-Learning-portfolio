c0b7a11aa686cbbbc0e355b0d71825f9f76bde1e
rainhard-obura <obura.rainhard@gmail.com>
2025-03-11 16:07:09 +0300

    added the python scripts

---
+++ DS_ML_projects/flood_prediction_model_deployment/model.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+import joblib
+
+def load_model():
+    model = tf.keras.models.load_model('flood_mode.h5')
+    return model
+
+def preprocess_input(precipitation, image):
+    precipitation = (precipitation - np.mean(precipitation)) / np.std(precipitation)
+    #Normalize the image 
+    image = image.astype(np.float32) / 255.0
+
+    return precipitation, image
